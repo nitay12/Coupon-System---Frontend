65d999b9869025b322199ba9541d243f34ee43e0
Nitay Caspi <NitayCaspi12@gmail.com>
2022-10-02 01:34:05 +0300

    create all companies page

--- src/Components/AdminArea/AllCompanies/AllCompanies.tsx
+++ src/Components/AdminArea/AllCompanies/AllCompanies.tsx
@@ -1,5 +1,29 @@
-const AllCompanies = ():JSX.Element => {
-  return <div>AllCompanies</div>;
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { CompanyModel } from "../../../Models/UserModel";
+import adminService from "../../../Services/AdminService";
+import { CompaniesTable } from "../CompaniesTable";
+import notificationService from "../../../Services/NotificationService";
+
+
+const AllCompanies = (): JSX.Element => {
+  const [companies, setCompanies] = useState<CompanyModel[]>([]);
+  useEffect(() => {
+    console.log(axios.defaults.headers);
+    adminService
+      .fetchCompanies()
+      .then((companies) => {
+        setCompanies(companies);
+      })
+      .catch((err) => notificationService.error(err));
+  }, []);
+
+  return (
+    <>
+      <h1>All Companies</h1>
+      <CompaniesTable companies={companies} />
+    </>
+  );
 };
 
 export default AllCompanies;
