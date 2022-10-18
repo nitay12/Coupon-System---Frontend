84267e3437f049607f10c8374259337782b29510
Nitay Caspi <NitayCaspi12@gmail.com>
2022-10-18 22:52:04 +0300

    fix jwt session

--- src/Redux/AuthState.ts
+++ src/Redux/AuthState.ts
@@ -1,66 +1,69 @@
+import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { createStore } from "redux";
 import { BaseUserModel } from "../Models/UserModel";
 
 // 1. State
 export class AuthState {
+  public token: string = null;
+  public user: BaseUserModel = null;
 
-    public token: string = null;
-    public user: BaseUserModel = null;
-
-    public constructor() {
-        this.token = localStorage.getItem("token");
-        if(this.token) {
-            const container: { user: BaseUserModel } = jwtDecode(this.token);
-            this.user = container.user;
-        }
+  public constructor() {
+    this.token = localStorage.getItem("token");
+    if (this.token) {
+      console.log(this.token);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${this.token}`;
+      const container: BaseUserModel = jwtDecode(this.token);
+      this.user = container;
+      console.log(this.user);
     }
-
+  }
 }
 
 // 2. Action Type
 export enum AuthActionType {
-    Login,
-    Logout
+  Login,
+  Logout,
 }
 
 // 3. Action
 export interface AuthAction {
-    type: AuthActionType;
-    payload?: string;
+  type: AuthActionType;
+  payload?: string;
 }
 
 // 4. Action Creators
 export function loginAction(token: string): AuthAction {
-    return { type: AuthActionType.Login, payload: token };
+  return { type: AuthActionType.Login, payload: token };
 }
 export function logoutAction(): AuthAction {
-    return { type: AuthActionType.Logout };
+  return { type: AuthActionType.Logout };
 }
 
 // 5. Reducer
-export function authReducer(currentState = new AuthState(), action: AuthAction): AuthState {
+export function authReducer(
+  currentState = new AuthState(),
+  action: AuthAction
+): AuthState {
+  const newState = { ...currentState };
 
-    const newState = { ...currentState };
+  switch (action.type) {
+    case AuthActionType.Login:
+      newState.token = action.payload;
+      const container: BaseUserModel = jwtDecode(newState.token);
+      newState.user = container;
+      console.log(newState.user);
+      localStorage.setItem("token", newState.token);
+      break;
 
-    switch (action.type) {
+    case AuthActionType.Logout:
+      newState.token = null;
+      newState.user = null;
+      localStorage.removeItem("token");
+      break;
+  }
 
-        case AuthActionType.Login:
-            newState.token = action.payload;
-            const container: BaseUserModel = jwtDecode(newState.token);
-            newState.user = container;
-            console.log(newState.user)
-            localStorage.setItem("token", newState.token);
-            break;
-
-        case AuthActionType.Logout:
-            newState.token = null;
-            newState.user = null;
-            localStorage.removeItem("token");
-            break;
-    }
-
-    return newState;
+  return newState;
 }
 
 // 6. Store
